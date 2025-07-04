d7953f8b318bbac0c680acf2814c0adfc7aa6fdf
Sanghoon Lee <gcccompil3r@gmail.com>
2025-07-04 14:11:45 +0900

    feat: FindBoardRequest 채워보기

---
+++ spring/demo/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Board;
+import com.example.demo.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// JPA를 사용해서 DB를 자동화 하는 경우
+// 무조건 아래와 같이 extends JpaRepository<Entity, Long> 형태를 추가하면 됩니다.
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
