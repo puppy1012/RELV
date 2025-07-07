1df8ed8da7a10c8bf237d5026ac14215fcdfeb0a
Sanghoon Lee <gcccompil3r@gmail.com>
2025-07-07 15:16:28 +0900

    feat: findByClassMemberField

---
+++ spring/demo/src/main/java/com/example/demo/controller/request/FindFruitNameRequest.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller.request;
+
+import lombok.*;
+
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FindFruitNameRequest {
+    String fruitName;
+}
